ba0c7037182eb1d7dbdfe3ab83517400b0bd86e3
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2024-07-17 14:52:08 +0200

    changes

--- main.py
+++ main.py
@@ -40,44 +40,59 @@ marked_locations = {
 def get_location(index):
     return list(location_to_state.keys())[list(location_to_state.values())[index]]
 
-# def toggle_choosing_start():
-    # choose_end = False
-    # if choose_start == False:
-    #     choose_start = True
-    # else:
-    #     choose_start = False
+def toggle_choosing_start():
+    global choose_start, choose_end
+    choose_end = False
+    choose_start = not choose_start
+    btn_start.config(state="disabled")
+    btn_end.config(state="active")
 
-# def toggle_choosing_end():
-    # choose_start = False
-    # if choose_end == False:
-    #     choose_end = True
-    # else:
-    #     choose_end = False
-
-root = Tk()
-root.geometry('420x400')
-c = Canvas(root, width=400, height=300)
-root.configure(bg='#2E3440')
-c.configure(bg='#2E3440')
-# btn_start = Button(root, text='Start', width=8,
-#              height=1, bd='5', command=toggle_choosing_start)
-# btn_end = Button(root, text='End', width=8,
-#              height=1, bd='5', command=toggle_choosing_end)
- 
-# btn_start.place(x=0, y=315)
-# btn_end.place(x=0, y=345)
+def toggle_choosing_end():
+    global choose_start, choose_end
+    choose_start = False
+    choose_end = not choose_end
+    btn_end.config(state="disabled")
+    btn_start.config(state="active")
 
 def clicked(event):
+    global choose_start, choose_end
     if choose_start == False and choose_end == False:
         return
+    
     clicked_item = get_location(int(int((event.widget.find_closest(event.x, event.y)[0])-1)/2))
-    if marked_locations[clicked_item] == 0:
+    if choose_start:
+        for a in range(1,25,2):
+            if marked_locations[get_location(int((a-1)/2))] == 1:
+                marked_locations[get_location(int((a-1)/2))] = 0
+                c.itemconfig(a,fill="#4C566A")
         marked_locations[clicked_item] = 1
-        c.itemconfig(event.widget.find_closest(event.x, event.y), fill="#B48EAD")
-    else:
-        marked_locations[clicked_item] = 0
-        c.itemconfig(event.widget.find_closest(event.x, event.y), fill="#4C566A")
-    print(clicked_item)
+        c.itemconfig(event.widget.find_closest(event.x, event.y), fill="#A3BE8C")
+        btn_start.config(state="active")
+        choose_start = False
+
+    if choose_end:
+        for a in range(1,25,2):
+            if marked_locations[get_location(int((a-1)/2))] == 2:
+                marked_locations[get_location(int((a-1)/2))] = 0
+                c.itemconfig(a,fill="#4C566A")
+        marked_locations[clicked_item] = 2
+        c.itemconfig(event.widget.find_closest(event.x, event.y), fill="#EBCB8B")
+        btn_end.config(state="active")
+        choose_end = False
+
+root = Tk()
+root.geometry('420x450')
+c = Canvas(root, width=420, height=350)
+root.configure(bg='#2E3440')
+c.configure(bg='#2E3440')
+btn_start = Button(root, text='Choose Start', width=8,
+             height=1, bd='3', command=toggle_choosing_start)
+btn_end = Button(root, text='Choose End', width=8,
+             height=1, bd='3', command=toggle_choosing_end)
+
+ 
+btn_start.place(x=150, y=343)
+btn_end.place(x=150, y=373)
 
 actions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
@@ -115,30 +130,50 @@ for a in range(12):
     max_Q_value = np.max(Q[a, :])
     if max_Q_value > warehouse_map[y, x]:
         warehouse_map[y, x] = max_Q_value
-    print(x,y)
     text = get_location(a)
-    playbutton = c.create_rectangle(100*x, 100*y, 100*x+100, 100*y+100, fill="#4C566A",tags="playbutton")
-    playtext = c.create_text(100*x+50, 100*y+50, text=text, font=("Comic Sans MS", 18), fill="#ECEFF4",tags="playbutton-text")
-    c.tag_bind("playbutton","<Button-1>",clicked)
+    playbutton = c.create_rectangle(100*x+10, 100*y+10, 100*x+110, 100*y+110, fill="#4C566A",tags="playbutton")
+    playtext = c.create_text(100*x+60, 100*y+55, text=text, font=("Comic Sans MS", 18), fill="#ECEFF4",tags="playbutton-text")
+    c.tag_bind("playbutton", "<Button-1>", clicked)
+
+def XD(event):
+    print("XD")
+
+for a in range(1,3):
+    for b in range(4):
+        wallbutton = c.create_rectangle(100*b+10, 100*a, 100*b+110, 100*a+10, fill="green",tags="wallbutton")
+        c.tag_bind("wallbutton", "<Button-1>", XD)
+
+for a in range(1,4):
+    for b in range(3):
+        wallbutton = c.create_rectangle(100*a+5, 100*b+10, 100*a+15, 100*b+110, fill="green",tags="wallbutton")
+        c.tag_bind("wallbutton", "<Button-1>", XD)
 
 c.pack()
 
 print(warehouse_map.astype(int))
 # print(Q.astype(int))
 
-def route(start, end):
-    path = np.array([start])
-    start_state = location_to_state[start]
-    end_state = location_to_state[end]
-    next_state = start_state
-    while next_state != end_state:
-        next_state = np.argmax(Q[start_state])
-        location = get_location(next_state)
-        start_state=next_state
-        path = np.append(path, location)
-    print(path)
+def route():
+    print(marked_locations)
+    start = list(marked_locations.keys())[list(marked_locations.values()).index(1)]
+    end = list(marked_locations.keys())[list(marked_locations.values()).index(2)]
+    try:
+        path = np.array([start])
+        start_state = location_to_state[start]
+        end_state = location_to_state[end]
+        next_state = start_state
+        while next_state != end_state:
+            next_state = np.argmax(Q[start_state])
+            location = get_location(next_state)
+            start_state=next_state
+            path = np.append(path, location)
+        print(path)
+    except:
+        print("ERROR")
 
-route("J", "G")
+btn_route = Button(root, text='Draw route', width=8,
+             height=1, bd='3', command=route)
+btn_route.place(x=150, y=403)
 
 root.mainloop()
 
